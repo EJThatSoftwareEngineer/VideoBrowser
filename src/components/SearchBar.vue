8874228ac8e153d622be0b79b0b1cf4447cc869c
EJ <EJArellano@protonmail.com>
2018-07-30 22:47:56 +0800

    Implemented search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input @input="onInput" type="search" />
+    </div>
+</template>
+
+<script>
+export default {
+    name:'SearchBar',
+    methods: {
+        onInput:function(event){
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
